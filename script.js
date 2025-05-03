7e57a1868d5b470617cd25bd18cc479832daa3a3
shuaib <joy425180example.com>
2025-05-03 13:09:06 +0530

    update2

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function toggleMenu() {
       document.getElementById('sideMenu').classList.toggle('active');
     }
+let iconcard = document.querySelector(".iconcard");
+let body = document.querySelector("body");
+let closecard = document.querySelector(".close")
+
+iconcard.addEventListener('click',() => {
+body.classList.toggle('showCart')
+})
+
+closecard.addEventListener('click',() => {
+body.classList.toggle('showCart')
+})
+
